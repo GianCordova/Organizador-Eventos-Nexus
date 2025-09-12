664e3d7a407b4da465dff199bf159869ea486ac2
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-09-12 17:30:12 -0600

    Se subio el proyecto base

---
+++ proyecto_fn_nexus/src/test/java/org/nexus/proyecto_fn_nexus/ProyectoFnNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexus.proyecto_fn_nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFnNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
