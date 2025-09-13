7acbe938585ff64939b36afa39c8d8e7560d2f0c
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-09-13 11:04:26 -0600

    Primer commit de prueba y creacion de unas clases

---
+++ src/main/java/org/nexus/proyecto_fn_nexus/dominio/dto/RecursoDto.java
@@ -0,0 +1,5 @@
+package org.nexus.proyecto_fn_nexus.dominio.dto;
+
+public class RecursoDto {
+
+}
---
+++ src/main/java/org/nexus/proyecto_fn_nexus/dominio/repository/RecursoRepository.java
@@ -0,0 +1,8 @@
+package org.nexus.proyecto_fn_nexus.dominio.repository;
+
+import org.nexus.proyecto_fn_nexus.dominio.dto.RecursoDto;
+import org.nexus.proyecto_fn_nexus.dominio.dto.RecursoDto;
+
+public interface RecursoRepository {
+
+}
