2850790726ed421133df4862b756c9255558c8ac
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-09-16 08:17:57 -0600

    Se cambiaron unas cosas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,29 @@
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Sillas Plegables', 'Mobiliario', 50, 2.50);
+VALUES ('Sillas Plegables', 'FURNITURE', 50, 2.50);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Mesa de Coctel', 'Mobiliario', 20, 15.00);
+VALUES ('Mesa de Coctel', 'FURNITURE', 20, 15.00);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Carpa 10x10', 'Cubiertas y estructuras', 5, 250.00);
+VALUES ('Carpa 10x10', 'COVERS_AND_STRUCTURES', 5, 250.00);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Focos LED Vintage', 'Iluminación', 100, 1.75);
+VALUES ('Focos LED Vintage', 'LIGHTS', 100, 1.75);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Sistema de Sonido Básico', 'Sonido y multimedia', 3, 120.50);
+VALUES ('Sistema de Sonido Básico', 'SOUND_AND_MULTIMEDIA', 3, 120.50);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Proyector HD', 'Sonido y multimedia', 7, 75.00);
+VALUES ('Proyector HD', 'SOUND_AND_MULTIMEDIA', 7, 75.00);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Centro de Mesa Floral', 'Decoración', 30, 10.00);
+VALUES ('Centro de Mesa Floral', 'DECORATION', 30, 10.00);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Servicio de Catering', 'Cocina y catering', 1, 500.00);
+VALUES ('Servicio de Catering', 'KITCHEN_AND_CATERING', 1, 500.00);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Máquina de Burbujas', 'Entretenimiento', 2, 35.00);
+VALUES ('Máquina de Burbujas', 'ENTERTAINMENT', 2, 35.00);
 
 INSERT IGNORE INTO Recursos (nombreRecurso, categoria, stock, precioAlquiler)
-VALUES ('Generador Eléctrico', 'otro', 4, 95.00);
+VALUES ('Generador Eléctrico', 'OTHER', 4, 95.00);
